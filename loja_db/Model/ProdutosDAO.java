c33e08d9d36cf2ae8fd52060db5b037bde5facc2
Devonauta <oliveirateixeira653@gmail.com>
2025-04-01 10:15:30 -0300

    Update ProdutosDAO.java

--- loja_db/Model/ProdutosDAO.java
+++ loja_db/Model/ProdutosDAO.java
@@ -2,48 +2,56 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.loja_db.Model;
+package Model;
 
 /**
  *
  * @author VINICIUSDASILVATEIXE
  */
 
-import org.mindrot.jbcrypt.BCrypt;
+
 import java.sql.*;
+import java.util.List; 
+import java.util.ArrayList; 
 
 public class ProdutosDAO {
-    public boolean registrarUsuario(String usuario, String senha) {
-        String sql = "INSERT INTO usuarios (usuario, senha) VALUES (?, ?)";
-        String senhaHash = BCrypt.hashpw(senha, BCrypt.gensalt());
+    public Produtos registrarProduto(int id, String produto, String descricao, double preco, int quant) {
+        String sql = "INSERT INTO produtos (id, produto, descrição, preço, quantidade) VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = ConexaoLojadb.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, usuario);
-            stmt.setString(2, senhaHash);
+        try (Connection connection = Conexao.conectar();
+            PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, produto);
+            stmt.setString(2, descricao);
+            stmt.setDouble(3, preco);
+            stmt.setInt(4, quant);
             stmt.executeUpdate();
-            return true;
         } catch (SQLException e) {
             e.printStackTrace();
-            return false;
         }
+        return null;
     }
+    
+     public List<Produtos> listar() {
+        List<Produtos> produtos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos";
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
 
-    public boolean validarLogin(String usuario, String senha) {
-        String sql = "SELECT senha FROM produto WHERE Produtos = ?";
-
-        try (Connection conn = ConexaoLojadb.conectar();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, usuario);
-            ResultSet rs = stmt.executeQuery();
-            
-            if (rs.next()) {
-                return BCrypt.checkpw(senha, rs.getString("senha"));
+            while (rs.next()) {
+                Produtos produto = new Produtos();
+                produto.setId(rs.getInt("id"));
+                produto.setProduto(rs.getString("produto"));
+                produto.setPreco(rs.getDouble("preco"));
+                produto.setQuant(rs.getInt("quantidade"));
+                produtos.add(produto);
             }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return produtos;
     }
 }
-
+    
+   
