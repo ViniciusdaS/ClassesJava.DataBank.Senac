981ac53d7b4b84959ad9244a4ffebc157fbe751d
Devonauta <oliveirateixeira653@gmail.com>
2025-04-01 08:45:01 -0300

    Update ConexaoLojadb.java

--- loja_db/Model/ConexaoLojadb.java
+++ loja_db/Model/ConexaoLojadb.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.loja_db.Model;
+package Model;
 
 /**
  *
@@ -10,16 +10,16 @@ package com.mycompany.loja_db.Model;
  */
 import java.sql.*; 
 
-public class ConexaoLojadb {
-    private static final String url = "jdbc:mysql://localhost:3306/lojadb";
-    private static final String usuario = "root";
-    private static final String senha = "root";
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/lojagosling";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "root";
     private static Connection connection;
 
     public static Connection conectar() {
         try {
             if (connection == null || connection.isClosed()) {
-                connection = DriverManager.getConnection(url, usuario, senha);
+                connection = DriverManager.getConnection(URL, USUARIO, SENHA);
             }
             return connection;
         } catch (SQLException error) {
@@ -27,13 +27,11 @@ public class ConexaoLojadb {
         }
     }
 
-    private static void criarTabelaProdutos() {
-        String sql = "CREATE TABLE IF NOT EXISTS produtos (" +
+    private static void criarTabelaUsuarios() {
+        String sql = "CREATE TABLE IF NOT EXISTS usuarios (" +
                      "id INT AUTO_INCREMENT PRIMARY KEY, " +
                      "nome VARCHAR(100) NOT NULL, " +
-                     "descrição TEXT NOT NULL), " +
-                     "preço DECIMAL (10,2) NOT NULL, " +
-                     "quantidade INT NOT NULL";
+                     "senha VARCHAR(100) NOT NULL)";
                 try (Statement stmt = connection.createStatement()) {
             stmt.execute(sql);
         } catch (SQLException error) {
